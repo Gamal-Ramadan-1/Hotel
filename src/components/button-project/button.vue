8330400d7a62d549acffcadcf9719088638ac418
Gamal-Ramadan-1 <gamal.ramadan.fe@gmail.com>
2022-08-30 17:54:57 +0200

    modified all of fills in my project

---
+++ src/components/button-project/button.vue
@@ -0,0 +1,72 @@
+<template>
+  <button class="btn">
+    <span>{{ title }}</span>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    title: String,
+  },
+}
+</script>
+<style scoped lang="scss">
+.btn {
+  background-color: red;
+  color: white;
+  text-transform: uppercase;
+  padding: 15px;
+  border-radius: 0px;
+  border: none;
+  position: relative;
+  overflow: hidden;
+  transition: all 0.5s ease;
+  transform: scaleX(1);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  span {
+    z-index: 3;
+    transition: all 0.5s ease;
+  }
+}
+.btn::before {
+  content: "";
+  position: absolute;
+  width: 101%;
+  height: 101%;
+  z-index: -1;
+  left: 0px;
+  top: 0px;
+  background-color: green;
+  transition: transform 0.5s;
+  transition-timing-function: ease;
+  transition-timing-function: cubic-bezier(0.5, 1.6, 0.4, 0.7);
+  transform-origin: 0 0;
+  transform: scaleX(0);
+}
+.btn:hover:before {
+  z-index: 1;
+  transform: scaleX(1);
+}
+.btn:hover span {
+  color: black;
+}
+// @media (max-width: 769px) {
+//   button {
+//     width: 50%;
+//   }
+// }
+// @media (min-width: 426px) {
+//   button {
+//     width: 30%;
+//   }
+// }
+// @media (min-width: 769px) {
+//   button {
+//     width: 18%;
+//   }
+// }
+</style>
