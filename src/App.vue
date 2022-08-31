6c628447b4a7c8647f446cff5fd19b42455d337f
Gamal-Ramadan-1 <gamal.ramadan.fe@gmail.com>
2022-08-31 18:00:42 +0200

    Start in footer component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <Navbar />
+  <Footer />
 </template>
 <script>
 import Navbar from "./components/Header/NavbarComp.vue";
+import Footer from "./components/footer/Footer.vue";
 export default {
   name: "App",
-  components: { Navbar },
+  components: { Navbar, Footer },
 };
 </script>
 <style lang="scss">
