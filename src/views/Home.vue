7eb60952ad05c2393f6797a33a53141ef66c9365
Gamal-Ramadan-1 <gamal.ramadan.fe@gmail.com>
2022-09-02 00:22:46 +0200

    Start in Services section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,15 +2,17 @@
   <Slider />
   <CheckAvailability />
   <AboutUs />
+  <Services />
 </template>
 
 <script>
 import Slider from "@/components/Slider-Home/Slider.vue";
 import CheckAvailability from "@/components/Check-availability/CheckAvailability.vue";
 import AboutUs from "@/components/about-us-section/AboutUs.vue";
+import Services from "@/components/services-section/Services.vue";
 // @ is an alias to /src
 export default {
   name: "HomeView",
-  components: { Slider, CheckAvailability, AboutUs },
+  components: { Slider, CheckAvailability, AboutUs, Services },
 };
 </script>
