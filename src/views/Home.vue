af7eeb03bbcae5a2e8a804562515a913048ba105
Gamal-Ramadan-1 <gamal.ramadan.fe@gmail.com>
2022-09-02 14:47:53 +0200

    </>

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,7 @@
   <CheckAvailability />
   <AboutUs />
   <Services />
+  <HotelForVacation />
 </template>
 
 <script>
@@ -10,9 +11,16 @@ import Slider from "@/components/Slider-Home/Slider.vue";
 import CheckAvailability from "@/components/Check-availability/CheckAvailability.vue";
 import AboutUs from "@/components/about-us-section/AboutUs.vue";
 import Services from "@/components/services-section/Services.vue";
+import HotelForVacation from "@/components/hotel-for-vacation/HotelForVacation.vue";
 // @ is an alias to /src
 export default {
   name: "HomeView",
-  components: { Slider, CheckAvailability, AboutUs, Services },
+  components: {
+    Slider,
+    CheckAvailability,
+    AboutUs,
+    Services,
+    HotelForVacation,
+  },
 };
 </script>
