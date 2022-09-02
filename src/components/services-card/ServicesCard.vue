ffdfa8701b69eadc2abb8057389e99d267d81b47
Gamal-Ramadan-1 <gamal.ramadan.fe@gmail.com>
2022-09-02 14:37:58 +0200

    finished services section

---
+++ src/components/services-card/ServicesCard.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="service-card">
+    <a href="#">
+      <i :class="ClassIcon"></i>
+      <h5 class="text-capitalize">{{ title }}</h5>
+    </a>
+  </div>
+</template>
+<script>
+export default {
+  name: "ServicesCard",
+  props: {
+    title: String,
+    ClassIcon: String,
+  },
+};
+</script>
+<style lang="scss" scoped>
+@import "@/Scss/main.scss";
+a {
+  text-decoration: none;
+}
+.service-card {
+  border-radius: 50% 50% 50% 0%;
+  cursor: pointer;
+  background-color: #ffffff;
+  padding: 30px;
+  transition: all 0.2s ease-in-out;
+
+  i {
+    font-size: 70px;
+    color: $Main-Color;
+  }
+  h5 {
+    color: black;
+    font-weight: bolder;
+    margin-top: 20px;
+    font-size: 17px;
+  }
+}
+.service-card:hover {
+  background-color: $Main-Color;
+  color: white;
+  transition: all 0.2s ease-in-out;
+  i,
+  h5 {
+    color: white;
+  }
+}
+@media (max-width: 767px) {
+  .service-card {
+    border-radius: 0px;
+  }
+}
+</style>
--- src/components/services-section/Services.vue
+++ src/components/services-section/Services.vue
@@ -1,11 +1,67 @@
 <template>
-    
+  <section class="services">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 text-center head-of-services my-5">
+          <span>unwind services</span>
+          <h1>Explore Our Hotel Services</h1>
+        </div>
+      </div>
+      <div class="row mt-3 mb-5 pb-5 gy-4">
+        <div class="col-lg-2 col-md-3 col-sm-12 text-center">
+          <ServicesCard title="free wifi" ClassIcon="fa-solid fa-wifi pt-2" />
+        </div>
+        <div class="col-lg-2 col-md-3 col-sm-12 text-center">
+          <ServicesCard
+            title="easy booking"
+            ClassIcon="fa-solid fa-book pt-2"
+          />
+        </div>
+        <div class="col-lg-2 col-md-3 col-sm-12 text-center">
+          <ServicesCard title="restuarant" ClassIcon="fa-solid fa-house pt-2" />
+        </div>
+        <div class="col-lg-2 col-md-3 col-sm-12 text-center">
+          <ServicesCard
+            title="swimming pool"
+            ClassIcon="fa-solid fa-person-swimming pt-2"
+          />
+        </div>
+        <div class="col-lg-2 col-md-3 col-sm-12 text-center">
+          <ServicesCard
+            title="beauty & health"
+            ClassIcon="fa-solid fa-bowl-food pt-2"
+          />
+        </div>
+        <div class="col-lg-2 col-md-3 col-sm-12 text-center">
+          <ServicesCard
+            title="healp & support"
+            ClassIcon="fa-solid fa-handshake-angle pt-2"
+          />
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 <script>
+import ServicesCard from "../services-card/ServicesCard.vue";
 export default {
   name: "Services",
+  components: { ServicesCard },
 };
 </script>
 <style lang="scss" scoped>
 @import "@/Scss/main.scss";
+a {
+  text-decoration: none;
+}
+.head-of-services {
+  span {
+    color: $Main-Color;
+    font-weight: bolder;
+  }
+  h1 {
+    font-size: 50px;
+    font-weight: bolder;
+  }
+}
 </style>
