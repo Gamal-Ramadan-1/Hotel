f2f6a8214529dc687f007a7b3789c3bdf155d96e
Gamal-Ramadan-1 <gamal.ramadan.fe@gmail.com>
2022-09-05 12:04:21 +0200

    make component header to all sections

---
+++ src/components/Header to all sections/HeaderSetions.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="row mb-5 pb-3">
+    <div class="col-12 text-center">
+      <span class="text-uppercase fw-bold"> {{SmallHead}}</span>
+      <h1 class="text-capitalize">{{BigHead}}</h1>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "HeaderSections",
+  props: {
+    BigHead: String,
+    SmallHead: String,
+  },
+};
+</script>
+<style lang="scss" scoped>
+@import "@/Scss/main.scss";
+span {
+  color: $Main-Color;
+}
+h1 {
+  font-size: 50px;
+  font-weight: bolder;
+}
+</style>
