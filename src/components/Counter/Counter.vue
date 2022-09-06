3061f23f9ce1bdfcbd948d412f32213f191e4c08
Gamal-Ramadan-1 <gamal.ramadan.fe@gmail.com>
2022-09-06 14:57:24 +0200

    Finished Counter Section

--- src/components/Counter/Counter.vue
+++ src/components/Counter/Counter.vue
@@ -1,9 +1,111 @@
-<template></template>
+<template>
+  <section class="counter px-5">
+    <div class="container-fluid">
+      <div class="row mx-md-5">
+        <div class="col-lg-3 col-md-6 col-sm-12">
+          <div class="happy-guests text-center">
+            <h1
+              data-purecounter-start="0"
+              data-purecounter-end="389"
+              data-purecounter-decimals="2"
+              class="purecounter"
+            >
+              0
+            </h1>
+            <span class="d-block text-capitalize fw-bold"
+              ># of happy guests</span
+            >
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-12">
+          <div class="rooms text-center">
+            <h1
+              data-purecounter-start="0"
+              data-purecounter-end="320"
+              data-purecounter-duration="3"
+              class="purecounter"
+            >
+              0
+            </h1>
+            <span class="d-block text-capitalize fw-bold"># of rooms</span>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-12">
+          <div class="staffs text-center">
+            <h1
+              data-purecounter-start="0"
+              data-purecounter-end="1000"
+              data-purecounter-duration="4"
+              class="purecounter"
+            >
+              0
+            </h1>
+            <span class="d-block text-capitalize fw-bold"># of Staffs</span>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-12">
+          <div class="destination text-center">
+            <h1
+              data-purecounter-start="0"
+              data-purecounter-end="587"
+              class="purecounter"
+              data-purecounter-duration="5"
+            >
+              0
+            </h1>
+            <span class="d-block text-capitalize fw-bold"
+              ># of destination</span
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
 <script>
+import { onMounted } from "@vue/runtime-core";
 export default {
   name: "Counter",
+  setup() {
+    onMounted(() => {
+      new PureCounter();
+    });
+  },
 };
 </script>
 <style lang="scss" scoped>
 @import "@/Scss/main.scss";
+.counter {
+  background-color: $Black-Color;
+  margin-bottom: 60px;
+  .happy-guests,
+  .rooms,
+  .staffs,
+  .destination {
+    position: relative;
+    padding-top: 80px;
+    padding-bottom: 80px;
+    z-index: 0;
+    h1 {
+      color: $Main-Color;
+      font-weight: 900;
+    }
+    span {
+      font-size: 15px;
+      color: rgb(211, 210, 210);
+    }
+  }
+  .happy-guests::before,
+  .rooms::before,
+  .staffs::before,
+  .destination::before {
+    content: "";
+    position: absolute;
+    height: 100px;
+    width: 1px;
+    top: 0px;
+    z-index: -1;
+    background-color: rgb(60, 59, 59);
+  }
+}
 </style>
