a5d8294a2a4d3bebd893a8232939a3180ff4b675
Gamal-Ramadan-1 <gamal.ramadan.fe@gmail.com>
2022-09-07 20:40:07 +0200

    add Slider in Blog Page

---
+++ src/views/Blog.vue
@@ -0,0 +1,13 @@
+<template>
+  <Slider />
+</template>
+<script>
+import Slider from "@/components/Slider Blog/Slider.vue";
+export default {
+  name: "Blog",
+  components: { Slider },
+};
+</script>
+<style lang="scss" scoped>
+@import "@/Scss/main.scss";
+</style>
