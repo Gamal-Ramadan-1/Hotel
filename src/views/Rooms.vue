17679ef8d26b0123b76875605188d5acae34a475
Gamal-Ramadan-1 <gamal.ramadan.fe@gmail.com>
2022-09-07 20:30:15 +0200

    add Services Section in Room Page

--- src/views/Rooms.vue
+++ src/views/Rooms.vue
@@ -2,14 +2,16 @@
   <Slider />
   <CheckAvailability />
   <RoomsOfRoomsPage />
+  <Services BigHead="explore our hotel services" SmallHead="unwind services" />
 </template>
 <script>
 import Slider from "@/components/Slider-Room/Slider.vue";
 import CheckAvailability from "@/components/Check-availability/CheckAvailability.vue";
 import RoomsOfRoomsPage from "@/components/Rooms Of Rooms Page/RoomsOfRoomsPage.vue";
+import Services from "@/components/services-section/Services.vue";
 export default {
   name: "Rooms",
-  components: { Slider, CheckAvailability, RoomsOfRoomsPage },
+  components: { Slider, CheckAvailability, RoomsOfRoomsPage, Services },
 };
 </script>
 <style lang="scss" scoped>
