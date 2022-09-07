1e12769e08f2408faaa98d21e6773f4f6e5e774c
Gamal-Ramadan-1 <gamal.ramadan.fe@gmail.com>
2022-09-07 14:17:40 +0200

    Start in Slider Of About Page

---
+++ src/views/About.vue
@@ -0,0 +1,13 @@
+<template>
+  <Slider />
+</template>
+<script>
+import Slider from "@/components/Slider-About/Slider.vue";
+export default {
+  name: "About",
+  components: { Slider },
+};
+</script>
+<style lang="scss" scoped>
+@import "@/Scss/main.scss";
+</style>
