3ce6b59f83a86c36a388311f695d5a8f61ca8b2b
Gamal-Ramadan-1 <gamal.ramadan.fe@gmail.com>
2022-09-07 15:07:21 +0200

    Finished Slider in About Page

---
+++ src/components/Overlay/Overlay.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="overlay"></div>
+</template>
+<script>
+export default {
+  name: "Overlay",
+};
+</script>
+<style lang="scss" scoped>
+@import "@/Scss/main.scss";
+.overlay {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  z-index: -1;
+  opacity: 0.4;
+  background-color: $Black-Color;
+}
+</style>
