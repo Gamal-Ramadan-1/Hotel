841104b3d6144fa4400a2e392758541239a4992b
Gamal-Ramadan-1 <gamal.ramadan.fe@gmail.com>
2022-09-07 12:56:23 +0200

    Finished our Blog Section with create blog card component

---
+++ src/components/Blog Card/BlogCard.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="card">
+    <img :src="require(`@/imgs/${SrcImg}`)" class="card-img-top" />
+    <div class="card-body">
+      <div class="meta text-uppercase my-3 fw-bold">
+        <span class="me-3">admin</span>
+        <span class="me-3">{{ metadate }}</span>
+        <a href="#">{{ Comments }}</a>
+      </div>
+      <h4 class="card-title fw-bold mb-3">{{ TitleCard }}</h4>
+      <p class="card-text my-4 fw-bold">{{ TextCard }}</p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "BlogCard",
+  props: ["metadate", "Comments", "TitleCard", "TextCard", "SrcImg"],
+};
+</script>
+<style lang="scss" scoped>
+@import "@/Scss/main.scss";
+.card {
+  line-height: 1.7;
+  border-style: none;
+  .card-img-top {
+    height: 300px;
+    width: 100%;
+    object-fit: cover;
+  }
+  .card-body {
+    .meta {
+      span {
+        font-size: 13px;
+        color: rgb(148, 146, 146);
+      }
+      a {
+        text-decoration: none;
+        font-size: 13px;
+
+        color: $Main-Color;
+      }
+    }
+    .card-text {
+      color: rgb(148, 146, 146);
+    }
+  }
+}
+</style>
--- src/components/Our Blog Sectio/Our-Blog.vue
+++ src/components/Our Blog Sectio/Our-Blog.vue
@@ -1,9 +1,64 @@
-<template></template>
+<template>
+  <section class="our-blog">
+    <div class="container">
+      <HeaderSetions :BigHead="BigHead" :SmallHead="SmallHead" />
+      <div class="row">
+        <div class="col-lg-3 col-sm-12 col-md-6">
+          <BlogCard
+            metadate="dec. 23,2022"
+            Comments="3 comments"
+            TitleCard="Best Hotel Near Beach in Hawaii"
+            TextCard="A small river named Duden flows by their place and supplies it with the necessary regelialia."
+            SrcImg="ximage_1.jpg.pagespeed.ic.qa1-6EPlps.webp"
+          />
+        </div>
+        <div class="col-lg-3 col-sm-12 col-md-6">
+          <BlogCard
+            metadate="nov. 7,2022"
+            Comments="12 comments"
+            TitleCard="Best Hotel Near Beach in Hawaii"
+            TextCard="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor natus excepturi neque iusto!"
+            SrcImg="ximage_2.jpg.pagespeed.ic.sA2CEy2Wig.webp"
+          />
+        </div>
+        <div class="col-lg-3 col-sm-12 col-md-6">
+          <BlogCard
+            metadate="mar. 13,2022"
+            Comments="5 comments"
+            TitleCard="Best Hotel Near Beach in Hawaii"
+            TextCard="Deleniti iusto mollitia voluptates ut. Sequi enim suscipit at dolor velit modi dolores ipsum"
+            SrcImg="ximage_3.jpg.pagespeed.ic.BeWnPT5-TM.webp"
+          />
+        </div>
+        <div class="col-lg-3 col-sm-12 col-md-6">
+          <BlogCard
+            metadate="jun. 20,2022"
+            Comments="19 comments"
+            TitleCard="Best Hotel Near Beach in Hawaii"
+            TextCard="A small river named Duden flows by their place and supplies it with the necessary regelialia."
+            SrcImg="ximage_4.jpg.pagespeed.ic.0xFNn6r7lO.webp"
+          />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
 <script>
+import HeaderSetions from "../Header to all sections/HeaderSetions.vue";
+import BlogCard from "../Blog Card/BlogCard.vue";
 export default {
   name: "Our-Blog",
+  props: {
+    BigHead: String,
+    SmallHead: String,
+  },
+  components: { HeaderSetions, BlogCard },
 };
 </script>
 <style lang="scss" scoped>
 @import "@/Scss/main.scss";
+.our-blog{
+  margin-top: 100px;
+  margin-bottom: 100px;
+}
 </style>
