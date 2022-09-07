487c28929c94bdaf0b29b072a9be4fc3c77ab38f
Gamal-Ramadan-1 <gamal.ramadan.fe@gmail.com>
2022-09-07 15:21:51 +0200

    add About Section in About Page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,11 +1,13 @@
 <template>
   <Slider />
+  <AboutUs />
 </template>
 <script>
 import Slider from "@/components/Slider-About/Slider.vue";
+import AboutUs from "@/components/about-us-section/AboutUs.vue";
 export default {
   name: "About",
-  components: { Slider },
+  components: { Slider, AboutUs },
 };
 </script>
 <style lang="scss" scoped>
