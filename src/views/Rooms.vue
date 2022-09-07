e17cbc40c1e956bf6eee94c2a75bb7b0e24db262
Gamal-Ramadan-1 <gamal.ramadan.fe@gmail.com>
2022-09-07 18:51:51 +0200

    add CheckAvailability Section in Rooms Page

--- src/views/Rooms.vue
+++ src/views/Rooms.vue
@@ -1,11 +1,13 @@
 <template>
   <Slider />
+  <CheckAvailability />
 </template>
 <script>
 import Slider from "@/components/Slider-Room/Slider.vue";
+import CheckAvailability from "@/components/Check-availability/CheckAvailability.vue";
 export default {
   name: "Rooms",
-  components: { Slider },
+  components: { Slider, CheckAvailability },
 };
 </script>
 <style lang="scss" scoped>
