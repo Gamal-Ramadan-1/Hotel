d0706179f8e24c08474eeef5f0fba6ce52ab20e6
Gamal-Ramadan-1 <gamal.ramadan.fe@gmail.com>
2022-09-07 19:40:03 +0200

    Cut Of Card in FeaturedRoom Section and Handle it

---
+++ src/components/Card Rooms/CardRooms.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="card text-center">
+    <a href="#" v-if="SrcImgTop">
+      <img :src="require(`@/imgs/${SrcImgTop}`)" class="card-img-top" />
+    </a>
+    <div class="card-body py-5 px-4">
+      <h3 class="fw-bolder text-capitalize mb-4">{{ TitleCard }}</h3>
+      <p>
+        {{ TextCard }}
+      </p>
+      <div
+        class="
+          text-uppercase
+          d-flex
+          flex-nowrap
+          justify-content-center
+          align-items-center
+          fw-bolder
+        "
+      >
+        <span>{{ Price }}</span>
+        <a href="#" class="fw-bolder border ms-3 py-1 px-2">{{ TextBtn }}</a>
+      </div>
+    </div>
+    <a href="#" v-if="SrcImgBottom">
+      <img :src="require(`@/imgs/${SrcImgBottom}`)" class="card-img-bottom" />
+    </a>
+  </div>
+</template>
+<script>
+export default {
+  name: "CardRooms",
+  props: {
+    SrcImgTop: String,
+    TitleCard: String,
+    TextCard: String,
+    Price: String,
+    TextBtn: String,
+    SrcImgBottom: String,
+  },
+};
+</script>
+<style lang="scss" scoped>
+@import "@/Scss/main.scss";
+a {
+  text-decoration: none;
+  color: black;
+}
+span {
+  color: $Main-Color;
+}
+
+.card {
+  border-style: none;
+  border-radius: 0px;
+  transition: all 0.3s ease-in-out;
+
+  img {
+    border-style: none;
+    border-radius: 0px;
+    height: 300px;
+    a {
+      color: white;
+    }
+  }
+}
+.card:hover {
+  background-color: $Black-Color;
+  transition: all 0.3s ease-in-out;
+  color: white;
+  a {
+    color: white;
+  }
+  img {
+    opacity: 0.5;
+    transition: all 0.3s ease-in-out;
+  }
+}
+</style>
--- src/components/Featured Rooms Section/FeaturedRooms.vue
+++ src/components/Featured Rooms Section/FeaturedRooms.vue
@@ -4,94 +4,37 @@
       <HeaderSetions :BigHead="BigHead" :SmallHead="SmallHead" />
       <div class="row justify-content-md-center gy-4">
         <div class="col-lg-4 col-md-6 col-sm-12">
-          <div class="card text-center">
-            <div class="card-body py-5 px-4">
-              <h3 class="fw-bolder text-capitalize mb-4">suit room</h3>
-              <p>
-                Far far away, behind the word mountains, far from the countries
-                Vokalia and Consonantia, there live the blind texts.
-              </p>
-              <div
-                class="
-                  text-uppercase
-                  d-flex
-                  flex-nowrap
-                  justify-content-center
-                  align-items-center
-                  fw-bolder
-                "
-              >
-                <span>$450 /hight</span>
-                <a href="#" class="fw-bolder border ms-3 py-1 px-2">book now</a>
-              </div>
-            </div>
-            <a href="#">
-              <img
-                src="@/imgs/xroom-1.jpg.pagespeed.ic.2dbJCSi6kW.webp"
-                class="card-img-bottom"
-              />
-            </a>
-          </div>
+          <CardRooms
+            SrcImgBottom="xroom-1.jpg.pagespeed.ic.2dbJCSi6kW.webp"
+            Price="450 /hight"
+            TextBtn="book now"
+            TextCard="
+          Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.
+          "
+            TitleCard="suite room"
+          />
         </div>
         <div class="col-lg-4 col-md-6 col-sm-12">
-          <div class="card text-center">
-            <a href="#">
-              <img
-                src="@/imgs/xroom-2.jpg.pagespeed.ic.qQEQzEZcIj.webp"
-                class="card-img-bottom"
-              />
-            </a>
-            <div class="card-body py-5 px-4">
-              <h3 class="fw-bolder text-capitalize mb-4">family room</h3>
-              <p>
-                Far far away, behind the word mountains, far from the countries
-                Vokalia and Consonantia, there live the blind texts.
-              </p>
-              <div
-                class="
-                  text-uppercase
-                  d-flex
-                  flex-nowrap
-                  justify-content-center
-                  align-items-center
-                  fw-bolder
-                "
-              >
-                <span>$450 /hight</span>
-                <a href="#" class="fw-bolder border ms-3 py-1 px-2">book now</a>
-              </div>
-            </div>
-          </div>
+          <CardRooms
+            SrcImgTop="xroom-2.jpg.pagespeed.ic.qQEQzEZcIj.webp"
+            Price="450 /hight"
+            TextBtn="book now"
+            TextCard="
+          Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.
+          "
+            TitleCard="family room"
+          />
         </div>
         <div class="col-lg-4 col-md-6 col-sm-12 text-md-center">
-          <div class="card text-center">
-            <div class="card-body py-5 px-4">
-              <h3 class="fw-bolder text-capitalize mb-4">deluxe room</h3>
-              <p>
-                Far far away, behind the word mountains, far from the countries
-                Vokalia and Consonantia, there live the blind texts.
-              </p>
-              <div
-                class="
-                  text-uppercase
-                  d-flex
-                  flex-nowrap
-                  justify-content-center
-                  align-items-center
-                  fw-bolder
-                "
-              >
-                <span>$450 /hight</span>
-                <a href="#" class="fw-bolder border ms-3 py-1 px-2">book now</a>
-              </div>
-            </div>
-            <a href="#">
-              <img
-                src="@/imgs/xroom-3.jpg.pagespeed.ic.-BqmSKjZKz.webp"
-                class="card-img-bottom"
-              />
-            </a>
-          </div>
+          <CardRooms
+            SrcImgBottom="xroom-3.jpg.pagespeed.ic.-BqmSKjZKz.webp"
+            Price="450 /hight"
+            TextBtn="book now"
+            TextCard="
+          Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.
+          "
+            TitleCard="deluxe room"
+          />
         </div>
       </div>
     </div>
@@ -99,53 +42,20 @@
 </template>
 <script>
 import HeaderSetions from "../Header to all sections/HeaderSetions.vue";
+import CardRooms from "@/components/Card Rooms/CardRooms.vue";
 export default {
   name: "FeaturedRooms",
   props: {
     BigHead: String,
     SmallHead: String,
   },
-  components: { HeaderSetions },
+  components: { HeaderSetions, CardRooms },
 };
 </script>
 <style lang="scss" scoped>
 @import "@/Scss/main.scss";
-
-a {
-  text-decoration: none;
-  color: black;
-}
-span {
-  color: $Main-Color;
-}
 .featured-rooms {
   margin-top: 120px;
   margin-bottom: 150px;
-}
-.card {
-  border-style: none;
-  border-radius: 0px;
-  transition: all 0.3s ease-in-out;
-
-  img {
-    border-style: none;
-    border-radius: 0px;
-    height: 300px;
-    a {
-      color: white;
-    }
-  }
-}
-.card:hover {
-  background-color: $Black-Color;
-  transition: all 0.3s ease-in-out;
-  color: white;
-  a {
-    color: white;
-  }
-  img {
-    opacity: 0.5;
-    transition: all 0.3s ease-in-out;
-  }
 }
 </style>
