a425682b739ee6451eeb4314c36b82edf2d052ac
Gamal-Ramadan-1 <gamal.ramadan.fe@gmail.com>
2022-09-07 15:49:13 +0200

    add TakeATour Section in About Page

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,14 +2,16 @@
   <Slider />
   <AboutUs />
   <HotelForVacation />
+  <TakeATour />
 </template>
 <script>
 import Slider from "@/components/Slider-About/Slider.vue";
 import AboutUs from "@/components/about-us-section/AboutUs.vue";
 import HotelForVacation from "@/components/hotel-for-vacation/HotelForVacation.vue";
+import TakeATour from "@/components/take a tour/TakeATour.vue";
 export default {
   name: "About",
-  components: { Slider, AboutUs, HotelForVacation },
+  components: { Slider, AboutUs, HotelForVacation, TakeATour },
 };
 </script>
 <style lang="scss" scoped>
