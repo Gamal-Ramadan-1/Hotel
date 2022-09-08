7be664ebc52039ecede0e4d1b919d1b2c553043f
Gamal-Ramadan-1 <gamal.ramadan.fe@gmail.com>
2022-09-08 12:56:04 +0200

    add Gallary Section in Resto&Bar Page

--- src/views/Reseto&Bar.vue
+++ src/views/Reseto&Bar.vue
@@ -1,13 +1,15 @@
 <template>
   <Slider />
   <RestoBarSection />
+  <Gallary />
 </template>
 <script>
 import Slider from "@/components/Slider Resto&Bar/Slider.vue";
 import RestoBarSection from "@/components/restobar section/RestoBarSection.vue";
+import Gallary from "@/components/Gallary/Gallary.vue";
 export default {
   name: "Reseto&Bar",
-  components: { Slider, RestoBarSection},
+  components: { Slider, RestoBarSection, Gallary },
 };
 </script>
 <style lang="scss" scoped>
