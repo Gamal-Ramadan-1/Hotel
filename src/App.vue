f518fec7246a7a9f143da4942ce8ef55bec9c393
Gamal-Ramadan-1 <gamal.ramadan.fe@gmail.com>
2022-09-27 15:35:09 +0200

    add button up to top

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <Navbar />
   <Footer />
+  <Up />
 </template>
 <script>
 import Navbar from "./components/Header/NavbarComp.vue";
 import Footer from "./components/footer/Footer.vue";
+import Up from "./components/Up To Top/Up.vue";
 export default {
   name: "App",
-  components: { Navbar, Footer },
+  components: { Navbar, Footer, Up },
 };
 </script>
 <style lang="scss">
---
+++ src/components/Up To Top/Up.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="up-to-top" @click="toTop()" v-show="scY > 200">TOP</div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      scY: 0,
+    };
+  },
+  mounted() {
+    window.addEventListener("scroll", this.handleScroll);
+  },
+  methods: {
+    handleScroll: function () {
+      this.scY = window.scrollY;
+    },
+    toTop: function () {
+      window.scrollTo(0, 0);
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+@import "@/Scss/main.scss";
+#up-to-top {
+  position: fixed;
+  z-index: 1000;
+  background-color: $Main-Color;
+  bottom: 20px;
+  right: 10px;
+  padding: 10px 20px;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+  color: white;
+}
+#up-to-top:hover {
+  color: black;
+  transition: all 0.3s ease-in-out;
+}
+</style>
